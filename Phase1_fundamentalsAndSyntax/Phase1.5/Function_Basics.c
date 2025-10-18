e2545943296eac43f7002ed58935e6ead703ab4c
Emanuel Jose <emanueljose0522@hotmail.com>
2025-10-17 22:05:29 -0400

    void function

--- Phase1_fundamentalsAndSyntax/Phase1.5/Function_Basics.c
+++ Phase1_fundamentalsAndSyntax/Phase1.5/Function_Basics.c
@@ -1,6 +1,7 @@
 #include <stdlib.h>
 #include <stdio.h>
 
+
 int add(int a, int b)
 {
     return a + b;
@@ -11,6 +12,11 @@ int subtract(int a, int b)
     return a - b;
 }
 
+//void not retrun value
+void print_message(const char *msg)
+{
+    printf("%s\n", msg);
+}
 int main()
 {
     int res = add(2, 4);
@@ -18,5 +24,6 @@ int main()
 
     int res2 = subtract(2, 4);
     printf("%d\n", res2);
+    print_message("Hello");
     return 0;
 }
