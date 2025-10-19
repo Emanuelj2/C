f1b44c81964a4e63bc788d10c66360f59eb911ec
emanul <emnaueljose0522@gmail.com>
2025-10-19 19:47:35 -0400

    added more array functions

--- Phase1_fundamentalsAndSyntax/Exercise1.4_Arrays_and_Strings/Arrays_And_Functions.c
+++ Phase1_fundamentalsAndSyntax/Exercise1.4_Arrays_and_Strings/Arrays_And_Functions.c
@@ -1,8 +1,12 @@
 #include <stdio.h>
 #include <stdlib.h>
+void print_array(int arr[], int size);
 int find_max(int arr[], int size);
 int find_max_optimized(int arr[], int size);
 int count_evens(int arr[], int size);
+int* reverse_array(int *arr, int size);
+int binary_search(int arr[], int size, int target);
+void bubble_sort(int arr[], int size);
 
 int main()
 {
@@ -14,10 +18,30 @@ int main()
     int max_val_res2 = find_max_optimized(arr, size);
     printf("The max value in the array is: %d\n", max_val_res2);
     int even_count_res = count_evens(arr, size);
-    printf("NUmber of even numbers in the arr: %d", even_count_res);
+    printf("Number of even numbers in the arr: %d\n", even_count_res);
+    int *reverse_array_res = reverse_array(arr, size);
+    printf("reveres array: ");
+    print_array(reverse_array_res, size);
+    free(reverse_array_res);
+    bubble_sort(arr, size);
+    printf("Sorted array (bubble sort): ");
+    print_array(arr, size);
+    int target = 6;
+    int binary_search_res = binary_search(arr, size, target);
+    printf("binary search for number %d fount at index : %d", target, binary_search_res);
+    
 
     return 0;
 }
+void print_array(int arr[], int size)
+{
+    for(int i = 0; i < size; i++)
+    {
+        printf("%d ", arr[i]);
+    }
+    printf("\n");
+}
+
 
 //O(n) time complexity
 int find_max(int arr[], int size)
@@ -64,4 +88,73 @@ int count_evens(int arr[], int size)
     }
 
     return num_of_evens;
+}
+
+int* reverse_array(int *arr, int size)
+{
+    //allocate memory for the new array
+    int* res = (int*)malloc(size * sizeof(int));
+
+    //check if malloc succeeded
+    if(res == NULL)
+    {
+        return NULL;
+    }
+
+    int left = 0;
+    int right = size - 1;
+
+    while(left < size)
+    {
+        res[left] = arr[right];
+        left++;
+        right--;
+
+    }
+
+    return res;
+}
+
+int binary_search(int arr[], int size, int target)
+{
+    //binary search
+    //note the array is already sorted
+    int low = 0;
+    int high = size - 1;
+
+    while(low <= high)
+    {
+        int middle = low + (high - low)/2;
+
+        if(arr[middle] == target)
+        {
+            return middle;
+        }
+
+        if(arr[middle] > target)
+        {
+            high = middle - 1;
+        }
+        else
+        {
+            low = middle + 1;
+        }
+    }
+    return - 1;
+}
+
+void bubble_sort(int arr[], int size)
+{
+    for(int i = 0; i < size - 1; i++)
+    {
+        for(int j = 0; j < size - i - 1; j++)
+        {
+            if(arr[j] > arr[j + 1])
+            {
+                int temp = arr[j];
+                arr[j] = arr[j + 1];
+                arr[j + 1] = temp;
+            }
+        }
+    }
 }
