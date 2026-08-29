f69b314f0a1808ca92a2701eb3a048a06ad56bd8
Emanuel Jose <emanueljose0522@gmail.com>
2026-08-29 19:33:35 -0400

    able to display the UI

--- PROJECTS/Nodes.c
+++ PROJECTS/Nodes.c
@@ -1,7 +1,33 @@
 #include "stdio.h"
+#include "raylib.h"
 
-int main()
+//window parameters
+#define WIN_W 800
+#define WIN_H 500
+
+//font size
+#define NORMAL_TEXT 20 
+
+//make a node
+
+
+int main(void)
 {
-	puts("Hello world!");
+	InitWindow(WIN_W, WIN_H, "Nodes");
+
+	SetTargetFPS(60);
+	
+	while(!WindowShouldClose())
+	{
+		//update variables here
+
+		//draw here
+		BeginDrawing();
+			DrawText("hello world", WIN_W/2, WIN_H/2, NORMAL_TEXT, RED);
+		EndDrawing();
+	}
+
+	CloseWindow();
+	
 	return 0;
 }
