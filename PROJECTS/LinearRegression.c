9e25a5f3de045bb0de40bd06cfd8b3fb7fa9275b
Emanuel Jose <emanueljose0522@gmail.com>
2026-08-30 22:15:46 -0400

    added linear regression as well as being able to see the data points

--- PROJECTS/LinearRegression.c
+++ PROJECTS/LinearRegression.c
@@ -1,6 +1,8 @@
 #include <stdio.h>
 #include <raylib.h>
+#include <stdint.h>
 #include <stdlib.h>
+#include <time.h>
 
 #define WIDTH 500
 #define HEIGHT 500
@@ -8,6 +10,16 @@
 #define MAX_POINTS 10
 #define GRID_SPACING 20
 #define GRAPH_TOP 100
+
+
+typedef struct Point
+{
+	int32_t x;
+	int32_t y;
+}Point;
+
+Point points[MAX_POINTS];
+int32_t point_count = 0;
 
 void draw_grid()
 {
@@ -21,6 +33,8 @@ void draw_grid()
 		DrawLine(0, y, WIDTH, y, WHITE);
 	}
 }
+
+/*
 void draw_point()
 {
 	int32_t max_x_index = WIDTH / GRID_SPACING;
@@ -32,15 +46,91 @@ void draw_point()
         int32_t x = x_index * GRID_SPACING;
         int32_t y = GRAPH_TOP + y_index * GRID_SPACING;
 
+	//stores the cordinates of the random points
+	points[point_count].x = x;
+	points[point_count].y = y;
+	point_count++;
+
         DrawCircle(x, y, 5, RED);
 }
+*/
 
 void draw_points(int32_t n)
 {
+	int32_t max_x_index = WIDTH / GRID_SPACING;
+        int32_t max_y_index = (HEIGHT - GRAPH_TOP) / GRID_SPACING;
+	int32_t total = (max_x_index + 1) * (max_y_index + 1);
+
+	Point all_points[total];
+	int32_t idx = 0;
+	for(int32_t x_i = 0; x_i <= max_x_index; x_i++)
+	{
+		for(int32_t y_i = 0; y_i <= max_y_index; y_i++)
+		{
+			all_points[idx].x = x_i * GRID_SPACING;
+                        all_points[idx].y = GRAPH_TOP + y_i * GRID_SPACING;
+                        idx++;
+		}
+	}
+
+	for(int32_t i = 0; i < n; i++)
+        {
+                int32_t j = i + rand() % (total - i);
+                Point tmp = all_points[i];
+                all_points[i] = all_points[j];
+                all_points[j] = tmp;
+        }
+
+	point_count = 0;
 	for(int32_t i = 0; i < n; i++)
 	{
-		draw_point();
+		points[point_count] = all_points[i];
+		point_count++;
 	}
+}
+
+/* linear regression formula: Y =  B_0 + (B_1 * X)
+ * Y = dependent variable (outcome)
+ * X = independent variable (predictor)
+ * B_0 = the y intercept
+ * B_1 = slope of the regression line (coefficient for X)
+ * e = error term (residuals)
+ */
+
+void linear_regression(float *b_0, float *b_1)
+{
+	float x_sum = 0;
+	float y_sum = 0;
+
+	for(int32_t i = 0; i < point_count; i++)
+	{
+		x_sum += points[i].x;
+		y_sum += points[i].y;
+	}
+
+
+	float x_mean = x_sum/point_count;
+	float y_mean = y_sum/point_count;
+
+	float numerator = 0;
+	float denominator = 0;
+
+	for(int32_t i = 0; i < point_count; i++)
+	{
+		float dx = points[i].x - x_mean;
+		float dy = points[i].y - y_mean;
+		numerator += dx * dy;
+		denominator += dx * dx;
+	}
+	*b_1 = numerator/denominator;
+	*b_0 = y_mean - (*b_1) * x_mean;
+}
+
+void draw_regression_line(float b_0, float b_1)
+{
+	int32_t y_start = (int32_t)(b_0 + b_1 * 0);
+	int32_t y_end = (int32_t)(b_0 + b_1 * WIDTH);
+	DrawLine(0, y_start, WIDTH, y_end, GREEN);
 }
 
 bool draw_button(int32_t x_pos, int32_t y_pos, const char* text)
@@ -63,23 +153,67 @@ int main(void)
 {
 	InitWindow(WIDTH, HEIGHT, "Linear Regression");
 	SetTargetFPS(60);
+	srand((uint32_t)time(NULL));
 
-	draw_grid();
+	float b_0 = 0;
+	float b_1 = 0;
+	bool has_line = false;
+
+	//draw_grid();
 	while(!WindowShouldClose())
 	{
-		BeginDrawing(); 
+		BeginDrawing();
+
+	       		ClearBackground(BLACK);	
+			draw_grid();
 			
-			if(draw_button(20, 20, "Regenerate"))
+			int32_t max_y_index = (HEIGHT - GRAPH_TOP) / GRID_SPACING;
+
+			Vector2 mouse = GetMousePosition();
+			for(int32_t i = 0; i < point_count; i++)
+			{
+        			DrawCircle(points[i].x, points[i].y, 5, RED);
+
+        			if(CheckCollisionPointCircle(mouse, (Vector2){points[i].x, points[i].y}, 8))
+        			{
+                			int32_t col = points[i].x / GRID_SPACING;
+                			int32_t row_from_top = (points[i].y - GRAPH_TOP) / GRID_SPACING;
+                			int32_t row_from_bottom = max_y_index - row_from_top;
+
+                			char label[32];
+                			snprintf(label, sizeof(label), "(%d, %d)", col, row_from_bottom);
+
+                			int32_t font_size = 14;
+                			int32_t text_width = MeasureText(label, font_size);
+                			int32_t box_x = points[i].x + 10;
+                			int32_t box_y = points[i].y - 10;
+                			int32_t padding = 4;
+
+                			DrawRectangle(box_x - padding, box_y - padding, text_width + padding * 2, font_size + padding * 2, LIGHTGRAY);
+                			DrawText(label, box_x, box_y, font_size, BLACK);
+        			}
+			}
+
+			for(int32_t i = 0; i < point_count; i++)
+			{
+				DrawCircle(points[i].x, points[i].y, 5, RED);
+			}
+			if(has_line)
+			{
+				draw_regression_line(b_0, b_1);
+			}
+
+			if(draw_button(20, 20, "Generate"))
 			{
 				draw_points(MAX_POINTS);
+				linear_regression(&b_0, &b_1);
+				has_line = true;
 				TraceLog(LOG_INFO, "button clicked : generated 10 random data points");
 			}
 			
 		EndDrawing();
 
 	}
-
-	CloseWindow();
 
 	return 0;
 }
